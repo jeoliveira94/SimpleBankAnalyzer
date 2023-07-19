243ad08052a61d3306da417df73f9e03ac2a9b6f
José Oliveira <eduardo.jeso@gmail.com>
2023-07-18 22:00:30 -0300

    [improvement] Decreasing COUPLING by make components rely on abstractions rather concrete implementations

--- BankStatementsAnalyzer/src/main/java/BankStatementAnalyzer.java
+++ BankStatementsAnalyzer/src/main/java/BankStatementAnalyzer.java
@@ -7,19 +7,23 @@ import java.util.List;
 
 public class BankStatementAnalyzer {
     private static final String RESOURCES = "src/main/resources/";
-    private static final BankTransactionCSVParser bankStatementParser = new BankTransactionCSVParser();
 
     public static void main(final String... args) throws IOException {
-        final String fileName = args[0];
-        final Path path = Paths.get(RESOURCES + fileName);
-        final List<String> lines = Files.readAllLines(path);
-        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
-        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
-        collectSummary(bankStatementProcessor);
+        final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+        final BankStatementParser bankStatementParser = new BankStatementCSVParser();
 
+        bankStatementAnalyzer.analyze(args[0], bankStatementParser);
     }
 
-    private static void collectSummary(final BankStatementProcessor bankStatementProcessor) {
+    public void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException {
+        final Path path = Paths.get(RESOURCES + fileName);
+        final List<String> lines = Files.readAllLines(path);
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
+        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
+        collectSummary(bankStatementProcessor);
+    }
+
+    private void collectSummary(final BankStatementProcessor bankStatementProcessor) {
         System.out.println("The total for all transactions is "
                 + bankStatementProcessor.calculateTotalAmount());
 
