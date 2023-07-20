1c5179347ef17912ebe0f3fec4b790620f3effa8
José Oliveira <eduardo.jeso@gmail.com>
2023-07-20 19:41:08 -0300

    [feature] Added find transactions greater or equal a certain amount

--- BankStatementsAnalyzer/src/main/java/BankStatementProcessor.java
+++ BankStatementsAnalyzer/src/main/java/BankStatementProcessor.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankStatementProcessor {
@@ -59,5 +60,15 @@ public class BankStatementProcessor {
         }
 
         return minimumTransacion;
+    }
+
+    public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            if(bankTransaction.getAmount() >= amount) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
     }
 }
